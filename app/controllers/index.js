a40076cbdd9c2196c7fea15f1a5c18145ff62a98
sandeepm212 <mandrs01@stm-mandrs-mr.corp.pvt>
2015-07-20 14:35:49 -0700

    GR

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,20 +1,20 @@
 var net = require("/net");
 
-var products = null;
+
 Ti.API.info("Make REST Call");
 net.download({
 	url: 'https://raw.githubusercontent.com/sandeepm212/appcelerator/master/products.json',
     type: 'json',
-    success: function (success) {
-        products = success;
-        Ti.API.info(success);
+    success: function (productsInfo) {
+        $.products.sections[0].setItems(productsInfo);
+        Ti.API.info(productsInfo);
     },
     error: function (error) {
         Ti.API.info(error);
     }
 });
 
-$.products.sections[0].setItems(products);
+
 
 (function () {
 	Ti.API.info("Make REST Call");	
